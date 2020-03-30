4179d31b1fddaaba842f65df861ac264f1c67570
Angelo06 <angelo06@ucm.es>
2020-03-30 20:28:49 +0200

    Propiedad de soltar un power up añadida a las vasijas

---
+++ Assets/Scripts/PowerUps/DropPowerUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPowerUp : MonoBehaviour
+{
+    public GameObject PowerUp;
+    private void OnDestroy()
+    {
+        if (PowerUp != null) { Instantiate<GameObject>(PowerUp, transform.position, Quaternion.identity); }
+    }
+}
