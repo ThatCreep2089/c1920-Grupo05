94137a6403bcd802bac511a120565b22f3443c2c
Angelo06 <angelo06@ucm.es>
2020-04-01 23:08:34 +0200

    Pues ya esta lo del disparo

--- Assets/Scripts/Player/Invulnerable.cs
+++ Assets/Scripts/Player/Invulnerable.cs
@@ -6,11 +6,13 @@ public class Invulnerable : MonoBehaviour
 {
     //Cuando el jugador recibe daño, se desavtica su componente de vida, por lo que no se le puede hacer daño//
     Vida player;
+    Disparar disparo;
     double timer = 0;
     public int TiempoInvulneravilidad;
     private void Awake()
     {
         player = GetComponent<Vida>();
+        disparo = GetComponent<Disparar>();
     }
     private void Update()
     {
@@ -20,9 +22,11 @@ public class Invulnerable : MonoBehaviour
     private void OnEnable()
     {
         player.enabled = false;
+        disparo.enabled = false;
     }
     private void OnDisable()
     {
         player.enabled = true;
+        disparo.enabled = true;
     }
 }
