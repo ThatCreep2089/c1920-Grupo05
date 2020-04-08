e06f267717aa8530def5e6f181a355d1af5792ca
Javier Meitín Moreno <jmeitin@ucm.es>
2020-04-08 17:51:12 +0200

    Commit con la plataforma temporal

---
+++ Assets/Scripts/TrampasScripts/PlatformTemporal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTemporal : MonoBehaviour
+{
+    //Tiempo de vida de la Plataforma tras el impacto
+    public float tiempo;
+
+    float tiempoAuxiliar;
+        
+    private bool hayPlataforma = true;
+
+    private void Awake () 
+    {
+        tiempoAuxiliar = tiempo;
+    }
+    
+    void Update () 
+    { 
+        if (!hayPlataforma && tiempoAuxiliar >=1)         
+            tiempoAuxiliar -= Time.deltaTime;
+        
+
+        if (!hayPlataforma && tiempoAuxiliar < 1)
+            Destroy(gameObject);    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hayPlataforma = false;
+    }
+}
