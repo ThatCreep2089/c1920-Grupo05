1aa71ad4111a83bf95ee825c00e9b71ad03a4799
omaya01 <62616911+omaya01@users.noreply.github.com>
2020-04-20 20:16:05 +0200

    Cosas que se me habían olvidado

---
+++ ProjectileProperties.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ProjectileProperties : MonoBehaviour
+{
+	//Tiempo de vida del proyectil
+    public float tiempoProyectil;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+
+        /* Depuración */
+        //Debug.LogError("Collision ocurred with G.O. " + collision.gameObject + " in layer " + collision.gameObject.layer);
+        //Debug.LogError("In layer " + gameObject.layer);
+    }
+    void Update()
+    {
+        //Destruye el disparo tras tiempoProyectil. Básicamente el rango de disparo.
+        Destroy(gameObject, tiempoProyectil);
+    }
+
+    //aumenta el tiempo que tarda el proyectil en desaparecer
+    public void AumentaRango(float multiplicador)
+    {
+        tiempoProyectil *= multiplicador;
+    }
+}
