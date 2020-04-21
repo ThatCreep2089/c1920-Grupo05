9f8cdbd3cdade560e9d053d22abb1809f9ed21c8
adriac02 <adriac02@ucm.es>
2020-04-21 12:42:14 +0200

    Añadido el script de Danyo con Trigger (sin invulnerabilidad))

---
+++ Assets/Scripts/DanyoTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanyoTrigger : MonoBehaviour
+{
+	public int danyo;
+	Invulnerable playerInv;
+	float contador = 0f;
+	public float DPT = 4f;
+
+	private void OnTriggerStay2D(Collider2D collision) //Usamos un OnTriggerStay para poder hacer un contador//
+	{
+		GameObject player = collision.gameObject;
+		contador += Time.deltaTime;
+		if(contador >= DPT)
+		{
+			if (player.GetComponent<Vida>() != null && player.GetComponent<Vida>().enabled)
+			{   //Hace daño al personaje en cuestión//
+				player.GetComponent<Vida>().QuitaVida(danyo);
+
+				//Comprobamos que sea el jugador y le aplicamos daño e invulnerabilidad//
+				playerInv = player.GetComponent<Invulnerable>(); //AVISO: la invulnerabilidad no se activa//
+				if (player != null && playerInv != null)
+					playerInv.enabled = true;
+			}
+			contador = 0;
+		}
+	}
+}
