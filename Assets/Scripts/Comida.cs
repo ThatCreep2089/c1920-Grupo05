8dfc1f8ed9fbc9f48294fd6076900a44b6fbeb0b
adriac02 <adriac02@ucm.es>
2020-04-22 13:15:31 +0200

    Añadida comida que regenera vida

---
+++ Assets/Scripts/Comida.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comida : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Vida>() != null)
+        {
+            if(collision.gameObject.GetComponent<Vida>().salud + 1 <= 6)
+            {
+                collision.gameObject.GetComponent<Vida>().salud++;
+                UIManager.instance.AddCorazon(1);
+            }
+            else if(collision.gameObject.GetComponentInChildren<PlayerMovement>() != null)
+            {
+                collision.gameObject.GetComponentInChildren<PlayerMovement>().velocityReduction = 1;
+            }
+        }
+        Destroy(gameObject);
+    }
+}
