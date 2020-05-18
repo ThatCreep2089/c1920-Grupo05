7aaab9d5170a274afbf10230211c5be617baa970
Joseda <jrave@ucm.es>
2020-05-18 15:09:32 +0200

    Cambios en los Menús

--- Assets/Scripts/Managers/MainMenuController.cs
+++ Assets/Scripts/Managers/MainMenuController.cs
@@ -1,13 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement; //IMPORTANATE
 
 public class MainMenuController : MonoBehaviour
 {
-    //CUANDO SE PRESIONA EL BOTON ==> SE LLAMA A ESTE METODO
-    public void StartGame ()
-    {
-        SceneManager.LoadScene("TileMap_Avaricia");
-    }
+	[SerializeField] float time = 0f;
+	GameObject play, settings, quit;
+	Animator anim;
+	float timer;
+	bool bookOpened = false;
+
+	private void Awake()
+	{
+		anim = GetComponentInChildren<Animator>();
+
+		timer = time;
+
+		play = transform.GetChild(3).gameObject;
+		settings = transform.GetChild(4).gameObject;
+		quit = transform.GetChild(5).gameObject;
+	}
+	
+	private void Update()
+	{
+		if(Input.anyKeyDown)
+		{
+			anim.SetTrigger("OpenBook");
+			bookOpened = true;
+		}
+
+		if(bookOpened)
+		{
+			//Contador
+			timer -= Time.deltaTime;
+
+			if (timer <= 0)
+			{
+				//Activamos los botones
+				play.SetActive(true);
+				settings.SetActive(true);
+				quit.SetActive(true);
+
+				timer = time;
+				bookOpened = false;
+			}
+		}
+		
+	}
+
+	//CUANDO SE PRESIONA EL BOTON ==> SE LLAMA A ESTE METODO
+	public void StartGame()
+	{
+		SceneManager.LoadScene("TileMap_Avaricia");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
 }
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public AudioMixer audio;
+
+   public void AjustarVolumen(float num)
+	{
+		audio.SetFloat("VolumenMaster", num);
+	}
+}
